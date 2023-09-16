8ba56373a89db918721fa1997b2a77ae486dea43
ruari636 <ruaritheakston@outlook.com>
2023-09-16 10:47:44 +0100

    Circular orbit check works, task 2 done

--- 3D_Grav.py
+++ 3D_Grav.py
@@ -3,12 +3,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# mass, spring constant, initial position and velocity
+# mass, G constant, initial position and velocity
+straight_down_descent = False
+
 m = 1
 M = 6.42 * (10 ** 23)
 r = np.array([10**6,0.0,0.0])
 v = np.array([0.0,0.0,0.0])
 G = 6.6743 * (10**-11)
+
+if not straight_down_descent: # do circular orbit
+    m = 1                     # F = 4285N at this distance
+    M = 6.42 * (10 ** 23)     # Therefore V initial = 20700m/s
+    r = np.array([10**5,0.0,0.0])
+    v = np.array([0.0,20700,0.0])
+    G = 6.6743 * (10**-11)
 
 # simulation time, timestep and time
 t_max = 170
@@ -36,15 +45,22 @@ for t in range(len(t_array) - 1):
     v = (1 / (2 * dt)) * (r - r_list[-2])
 
 # convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
-x_array = np.array(r_list)
+pos_array = np.array(r_list)
 v_array = np.array(v_list)
+dim_array = ["x", "y", "z"]
 
 # plot the position-time graph
 plt.figure(1)
 plt.clf()
 plt.xlabel('time (s)')
 plt.grid()
-plt.plot(t_array, x_array, label='x (m)')
-plt.plot(t_array, v_array, label='v (m/s)')
+for i in range(3):
+    cur_dim_pos_array = []
+    cur_dim_vel_array = []
+    for n in range(len(pos_array)):
+        cur_dim_pos_array.append(pos_array[n][i])
+        cur_dim_vel_array.append(v_array[n][i])
+    plt.plot(t_array, cur_dim_pos_array, label=f'{dim_array[i]} (m)')
+    plt.plot(t_array, cur_dim_vel_array, label=f'{dim_array[i]} (m/s)')
 plt.legend()
 plt.show()
